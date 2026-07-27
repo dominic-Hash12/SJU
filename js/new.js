ff42e15d5b1b66a55a06c516906729e89aa9d161
dominic-Hash12 <dominicdlima25@gmail.com>
2026-07-27 08:17:37 +0530

    Update new.js

--- js/new.js
+++ js/new.js
@@ -148,7 +148,7 @@ title:"Library",
 description:
 "Library with a vast collection of books and resources.",
 
-image:"../images/new/library.jpg",
+image:"../images/new/libraray.jpg",
 
 transition:
 "../videos/science/intro.mp4",
